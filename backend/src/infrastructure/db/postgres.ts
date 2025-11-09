449c95debb33b5e70392e2a51d11d8c664d427aa
Utkarsha Pandey <utkarshapandey2004@gmail.com>
2025-11-09 20:03:04 +0530

    completed the setup of beekeeper and provided the seed data

--- backend/src/infrastructure/db/postgres.ts
+++ backend/src/infrastructure/db/postgres.ts
@@ -1,10 +1,11 @@
 import { Pool } from 'pg';
-
+import dotenv from 'dotenv';
+dotenv.config();
 // Make sure to set these environment variables!
 export const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'fueleu',
-  password: process.env.DB_PASSWORD || 'your_password',
+  password: process.env.PASSWORD ,
   port: parseInt(process.env.DB_PORT || '5432'),
 });
